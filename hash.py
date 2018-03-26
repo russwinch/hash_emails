82b66f2aaa7b663beb837307229667c2f89c4313
russ <russell.winch@gmail.com>
2018-03-26 13:37:32 +0100

    Added function to ask the user for input and output files. Changed default algorithm to SHA256.

--- hash.py
+++ hash.py
@@ -1,4 +1,5 @@
 import hashlib
+import os
 import time
 
 
@@ -8,7 +9,7 @@ def hasher(text, algorithm):
     return m.hexdigest()
 
 
-def hashfile(file_in, file_out, algorithm='md5', anonymise=True):
+def hashfile(file_in, file_out, algorithm='sha256', anonymise=True):
     '''
     Reads the file line by line, appending the hashed email.
 
@@ -41,15 +42,28 @@ def hashfile(file_in, file_out, algorithm='md5', anonymise=True):
         total_lines, infile, outfile, total_time))
 
 
+def collect_files():
+    existing_outfile = False
+    infile = input("Path to source file > ")
+    if not os.access(infile, os.F_OK):
+        raise IOError("Source file not found, ensure path is correct")
+    outfile = input("Path to output file > ")
+    if os.access(outfile, os.F_OK):
+        existing_outfile = True
+        if input("File exists. OK to overwrite? [y/n] > ") != 'y':
+            raise IOError("""User declined to grant permission to overwrite
+                            existing file with 'y'""")
+    return infile, outfile, existing_outfile
+
+
 def clearfile(text_file):
     '''Opens the file in write mode which clears it.'''
     with open(text_file, mode='w'):
-        print('file {} cleared!'.format(text_file))
+        print('{} cleared!'.format(text_file))
 
 
 if __name__ == '__main__':
-    infile = 'testdata.csv'
-    outfile = 'outfile.csv'
-
-    clearfile(outfile)
+    infile, outfile, existing_outfile = collect_files()
+    if existing_outfile:
+        clearfile(outfile)
     hashfile(infile, outfile)  # , algorithm='sha256')  # , anonymise=False)
