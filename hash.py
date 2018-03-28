8898d5861c9ff0f8678248f69e0d8806d12dc5a3
russ <russell.winch@gmail.com>
2018-03-28 09:49:47 +0100

    Changed from input to command line api. Check for valid algorithm. Round total seconds.

--- hash.py
+++ hash.py
@@ -1,15 +1,19 @@
 import hashlib
 import os
+import sys
 import time
 
 
 def hasher(text, algorithm):
     '''Applies hashing to the string and returns the hex value.'''
+    if algorithm not in hashlib.algorithms_available:
+        raise AttributeError("Algorithm not supported")
+
     m = getattr(hashlib, algorithm)(str.encode(text))
     return m.hexdigest()
 
 
-def hashfile(file_in, file_out, algorithm='sha256', anonymise=True):
+def hashfile(file_in, file_out, algorithm, anonymise=True):
     '''
     Reads the file line by line, appending the hashed email.
 
@@ -38,28 +42,26 @@ def hashfile(file_in, file_out, algorithm='sha256', anonymise=True):
                 else:
                     out.write(hashed_email + '\n')
 
-    total_time = time.time() - start_time
-    print('{} lines processed from {} to {} in {} seconds'.format(
-        total_lines, infile, outfile, total_time))
+    total_time = round(time.time() - start_time, 6)
+    print('{} lines processed in {} seconds using {}'.format(
+        total_lines, total_time, algorithm))
 
 
-def collect_files():
+def check_files(infile, outfile):
+    '''Checks files are valid and returns True if output file exists.'''
     existing_outfile = False
-    infile = input("Path to source file > ")
 
     if not os.access(infile, os.F_OK):
         raise IOError("Source file not found, ensure path is correct")
-    outfile = input("Path to output file > ")
 
     if os.access(outfile, os.F_OK):
         if infile == outfile:
-            raise IOError("Source and output file cannot be the same")
+            raise IOError("Source and output files cannot be the same")
         existing_outfile = True
         if input("File exists. OK to overwrite? [y/n] > ") != 'y':
             raise IOError("""User declined to grant permission to overwrite
                             existing file with 'y'""")
-
-    return infile, outfile, existing_outfile
+    return existing_outfile
 
 
 def clearfile(text_file):
@@ -69,7 +71,19 @@ def clearfile(text_file):
 
 
 if __name__ == '__main__':
-    infile, outfile, existing_outfile = collect_files()
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        raise IOError("""Source and output file paths are missing.
+                Usage: hash.py <source> <output> <algorithm>[optional]""")
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    if len(sys.argv) == 4:
+        algorithm = sys.argv[3].lower()
+    else:
+        algorithm = 'sha256'  # default to sha256
+
+    existing_outfile = check_files(infile, outfile)
     if existing_outfile:
         clearfile(outfile)
-    hashfile(infile, outfile)
+
+    hashfile(infile, outfile, algorithm)
